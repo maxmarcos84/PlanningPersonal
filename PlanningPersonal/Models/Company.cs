edf21656dbee1f00d11b137d74202208473c85ec
maxmarcos84 <maxmarcos84@gmail.com>
2022-10-28 23:34:29 -0300

    Agregar archivos de proyecto.

---
+++ PlanningPersonal/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlanningPersonal.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }        
+        [Required]
+        public string Name { get; set; } 
+        public string? Description { get; set; }
+        public string? Address { get; set; }
+
+
+    }
+}
---
+++ PlanningPersonal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlanningPersonal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
